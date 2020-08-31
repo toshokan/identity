a2a09d7ca688454cbf7172ab222c63514732ba54
toshokan <toshokan@shojigate.net>
2020-08-30 23:56:36 -0400

    Move http to server namespace

--- src/clj/identity/server.clj
+++ src/clj/identity/server.clj
@@ -1,1 +1,27 @@
-(ns identity.server)
+(ns identity.server
+  (:use ring.adapter.jetty)
+  (:require [compojure.core :refer :all]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.util.response :as r]
+            [hiccup.page :refer [html5 include-js]]))
+
+(def home-page
+  (html5
+   [:html
+    [:head
+     [:meta {:charset "utf-8"}]
+     [:title "Identity"]]
+    [:body
+     [:div {:hidden true}]
+     [:div#root]
+     (include-js "js/app.js")]]))
+
+(defroutes app
+  (GET "/" [] home-page))
+
+(defn start []
+  (-> app
+      (wrap-resource "public")
+      (wrap-content-type)
+      (run-jetty {:port 8001})))
