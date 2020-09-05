17d61f3f6f13c877a65dcc088a23f0fcf01b1f8f
toshokan <toshokan@shojigate.net>
2020-09-05 10:02:37 -0400

    Fetch challenge info and view value

--- src/cljs/identity/core.cljs
+++ src/cljs/identity/core.cljs
@@ -1,21 +1,22 @@
 (ns identity.core
   (:require [reagent.dom :as rd]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [cljs.reader :refer [read-string]]))
 
 (defn startup []
   [:div [:h2 "Identity"]
-   [:p @(rf/subscribe [:challenge-value])]])
+   [:p @(rf/subscribe [:challenge-id])]])
 
 (rf/reg-event-db
  :init
  (fn [_ _]
    (let [element (.getElementById js/document "challenge")]
-     {:challenge (.-value element)})))
+     {:challenge (read-string (.-value element))})))
 
 (rf/reg-sub
- :challenge-value
+ :challenge-id
  (fn [db _]
-   (:challenge db)))
+   (get-in db [:challenge :id])))
 
 (defn mount-root []
   (let [element (.getElementById js/document "root")]
