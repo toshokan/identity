995fb8811545aa4eda707de3308d701c0092274c
toshokan <toshokan@shojigate.net>
2020-09-05 10:02:08 -0400

    Get challenge info before delivering SPA template

--- src/clj/identity/server.clj
+++ src/clj/identity/server.clj
@@ -5,25 +5,35 @@
             [ring.middleware.resource :refer [wrap-resource]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.util.response :as r]
+            [clj-http.client :as h]
+            [cheshire.core :as json]
             [hiccup.page :refer [html5 include-js]]))
 
-(defn home-page [challenge]
+(defn home-page [info]
   (html5
    [:html
     [:head
      [:meta {:charset "utf-8"}]
      [:title "Identity"]]
     [:body
-     [:input#challenge {:type "hidden" :value challenge}]
+     [:input#challenge {:type "hidden" :value info}]
      [:div#root]
      (include-js "js/app.js")]]))
 
+(defn challenge-info [challenge]
+  (some-> (str "http://localhost:8001/oauth/v1/challenge/" challenge)
+          (h/get {:accept :json})
+          :body
+          (json/parse-string true)))
+
 (defroutes app
-  (GET "/" [challenge] (home-page challenge)))
+  (GET "/" [challenge]
+       (let [info (challenge-info challenge)]
+         (home-page info))))
 
 (defn start []
   (-> app
       (wrap-resource "public")
       (wrap-content-type)
       (wrap-params)
-      (run-jetty {:port 8001})))
+      (run-jetty {:port 8002})))
