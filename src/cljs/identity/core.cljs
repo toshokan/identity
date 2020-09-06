c69437f0520383db24dd5cc7203441a9e9c12adb
toshokan <toshokan@shojigate.net>
2020-09-05 23:48:38 -0400

    Show info about the authorization request linked to the challenge

--- src/cljs/identity/core.cljs
+++ src/cljs/identity/core.cljs
@@ -4,12 +4,21 @@
             [cljs.reader :refer [read-string]]))
 
 (defn login-component []
-  [:form {:method "post"}
-   [:label "Name"
-    [:input {:name "user-id"}]]
-   [:label "Password"
-    [:input {:name "password" :type "password"}]]
-   [:button "Submit"]])
+  (let [challenge-info @(rf/subscribe [:challenge-info])]
+    [:div 
+     [:div (str "Your login is being requested on behalf of " (:client_id challenge-info))]
+     [:div "The requested scopes are:"]
+     [:ul
+      (for [scope (clojure.string/split (:scope challenge-info) #" ")]
+        [:li scope])]
+     [:br]
+     [:form {:method "post"}
+      [:input {:name "user-id" :placeholder "Username"}]
+      [:br]
+      [:input {:name "password" :type "password" :placeholder "Password"}]
+      [:br]
+      [:br]
+      [:button "Submit"]]]))
 
 (defn startup []
   [:div [:h2 "Identity"]
@@ -25,6 +34,11 @@
  :challenge-id
  (fn [db _]
    (get-in db [:challenge :id])))
+
+(rf/reg-sub
+ :challenge-info
+ (fn [db _]
+   (:challenge db)))
 
 (defn mount-root []
   (let [element (.getElementById js/document "root")]
